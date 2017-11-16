f9f11ce51b3f736c0afe8b569d44edeb40e83f82
allen4dev <alanaliagadev@gmail.com>
2017-11-15 22:59:29 -0400

    [Add] base structure

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const http = require('http');
-const express = require('express');
+
+const app = require('./server/app');
 
 const PORT = process.env.POST || 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Blog API' });
-});
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
-
-module.exports = app;
--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const expect = require('expect');
 
-const app = require('./../index');
+const app = require('./../app');
 
-describe('GET /', () => {
+describe('GET /api/users', () => {
   it('should return a JSON message', done => {
     request(app)
-      .get('/')
+      .get('/api/users')
       .expect(200)
       .expect(res => {
         expect(res.body.message).toBe('Blog API');
