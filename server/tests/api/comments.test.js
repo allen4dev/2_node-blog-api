86b2c26617c2a5db2d313e6bb1982720ec442ac7
allen4dev <alanaliagadev@gmail.com>
2017-11-18 11:54:08 -0400

    [ToDo] PUT /api/comments

--- server/tests/api/comments.test.js
+++ server/tests/api/comments.test.js
@@ -7,10 +7,14 @@ const { signToken } = require('./../../auth/helpers');
 const app = require('./../../app');
 
 const Post = mongoose.model('Post');
+const Comment = mongoose.model('Comment');
 const User = mongoose.model('User');
 
 const uid1 = new ObjectID();
 const uid2 = new ObjectID();
+
+const postId1 = new ObjectID();
+const postId2 = new ObjectID();
 
 const users = [
   {
@@ -31,15 +35,30 @@ const users = [
 
 const posts = [
   {
-    _id: new ObjectID(),
+    _id: postId1,
     title: 'Post title 1',
     description: 'Post description 1',
     author: uid1,
   },
   {
-    _id: new ObjectID(),
+    _id: postId2,
     title: 'Post title 2',
     description: 'Post description 2',
+    author: uid2,
+  },
+];
+
+const comments = [
+  {
+    _id: new ObjectID(),
+    content: 'comment one',
+    post: postId1,
+    author: uid1,
+  },
+  {
+    _id: new ObjectID(),
+    content: 'comment two',
+    post: postId2,
     author: uid2,
   },
 ];
@@ -55,7 +74,13 @@ describe.only('api comments', () => {
 
     const populatePosts = Post.remove({}).then(() => Post.insertMany(posts));
 
-    Promise.all([populateUsers, populatePosts]).then(() => done());
+    const populateComments = Comment.remove({}).then(() =>
+      Comment.insertMany(comments)
+    );
+
+    Promise.all([populateUsers, populatePosts, populateComments]).then(() =>
+      done()
+    );
   });
 
   describe('POST /api/comments', () => {
@@ -88,6 +113,30 @@ describe.only('api comments', () => {
         .post('/api/comments')
         .send({ content, postId })
         .expect(401)
+        .end(done);
+    });
+  });
+
+  describe('PUT /api/comments/:id', () => {
+    it('should update a comment if token is present', done => {
+      const author = uid1.toHexString();
+      const token = signToken({ _id: author });
+      const postId = posts[0]._id.toHexString();
+      const id = comments[0]._id.toHexString();
+      const content = 'New content for comment';
+
+      request(app)
+        .put(`/api/comments/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({ content, postId })
+        .expect(200)
+        .expect(res => {
+          const { comment } = res.body;
+
+          expect(comment.content).toBe(content);
+          expect(comment.author).toBe(author);
+          expect(comment.post).toBe(postId);
+        })
         .end(done);
     });
   });
